ebdc17f804d99550ca57ab60ec10071c3d48e5f1
DAniel <romerondan123@gmail.com>
2024-01-07 19:22:42 -0600

    "week 1"

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -1,9 +1,16 @@
 using System;
 
+
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep1 World!");
+        Console.Write("What is your first name ");// console write is imput 
+        string name =Console.ReadLine();
+
+         Console.Write("What is your last name ");// console write is imput 
+        string surname =Console.ReadLine();
+
+        Console.WriteLine($"Your name is {surname}, {name} {surname} ");
     }
 }
