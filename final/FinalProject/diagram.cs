06b11e088a904c8951841e19ab7017214df3a4fa
DAniel <romerondan123@gmail.com>
2024-02-17 08:24:12 -0600

    "Diagram"

---
+++ final/FinalProject/diagram.cs
@@ -0,0 +1,31 @@
+--------------------     |----------------|
+|   LibraryItem    |     |     Book       |
+|------------------|     |----------------|
+| -Title: string   |     | -ISBN: string  |
+| -Author: string  |     |----------------|
+| -Year: int       |     | +Display()     |
+|------------------|     1----------------|
+          ^
+          |
+          |
+|------------------|
+|     Library      |
+|------------------|
+| -items: List<>   |
+|------------------|
+| +AddItem()       |
+| +DisplayItems()  |
+| +SearchBookByTitle()|
+|------------------|
+
+
+// When the program whre create in the library abject  in the initial books crete the reateLibraryWithBooks
+
+// The program enters a loop in the RunLibraryMenu method, displaying a menu of options to the user.
+
+//Depending on the user's choice, the program either searches for a book by title, adds a new book to the library, or exits the program.
+
+
+//When adding a new book, the program prompts the user for details of the new book and creates a new Book object using the provided information.
+
+//The program continues to loop until the user chooses to exit.
